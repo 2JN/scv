19332e6b9d7ce58cb353e2e7b44cc211d8eaef50
2J-N <jesquivelnavas@gmail.com>
2016-11-10 14:42:33 -0600

    Agrega automatizacion de usuario para la instalacion del sistema.

--- server/main.js
+++ server/main.js
@@ -1,4 +1,17 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+
 import '../imports/api/empleados';
 import '../imports/api/dependencias';
 import '../imports/api/nombramientos';
 import '../imports/api/users';
+
+Meteor.startup(() => {
+  if (Meteor.users.find().count() === 0) {
+    Accounts.createUser({username: 'admin', password: '0jud!c!al'});
+    let uId = Meteor.users.findOne({username: 'admin'})._id;
+    Meteor.users.update({_id: uId}, {
+      $set: { admin: true }
+    })
+  }
+});
